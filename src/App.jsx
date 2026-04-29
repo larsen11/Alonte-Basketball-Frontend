8e013631c83da92439f07f66045f2c44da8df40b
AJ Larsen R. Reyes <reyes.ajlarsen@gmail.com>
2026-04-29 23:45:32 +0800

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NBAteamform from "./components/NBAteamform";
+
+function App(){
+  return(
+    <div>
+      <NBAteamform/>
+    </div>
+  );
+}
+
+export default App;
